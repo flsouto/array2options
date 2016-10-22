11e596dc519754f72a607849f32da0993b3618a5
Fabio Souto <fabiolimasouto@gmail.com>
2016-10-22 13:18:50 -0200

    create dataset2options function

---
+++ dataset2options.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Generates <option> tags to be inserted in an html select element
+ * The first value of each row is used as key and the second as label
+ *
+ * @param $dataset array array of rows
+ * @param $select mixed key to be selected
+*/
+function dataset2options($dataset, $select=null){
+	
+	$assoc = array();
+	foreach($dataset as $row){
+		$k = current($row);
+		$v = next($row);
+		$assoc[$k] = $v;
+	}
+
+	return assoc2options($dataset, $select);
+}
