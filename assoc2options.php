0680bbbc757423f3ae617d673142d43bf024f002
Fabio Souto <fabiolimasouto@gmail.com>
2016-10-22 13:12:03 -0200

    create assoc2options function

---
+++ assoc2options.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Generates <option> tags to be inserted in an html select element
+ *
+ * @param $assoc array associative array
+ * @param $select mixed key to be selected
+*/
+function assoc2options($assoc, $select=null){
+
+	$options = '';
+	foreach($assoc as $k => $v){
+		$select = !is_null($select) && $select==$k;
+		$options .= '<option value="'.$k.'"'.$select.'>'.$v.'</option>';
+	}
+
+	return $options;
+
+}
